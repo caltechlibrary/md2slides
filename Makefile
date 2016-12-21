e06ddc933b8e242f3bd58f8db53f8fe9a1ca64f6
R. S. Doiel <rsdoiel@gmail.com>
2016-12-21 11:31:01 -0800

    Quick save

--- Makefile
+++ Makefile
@@ -2,6 +2,12 @@
 # Makefile to compile mkslides for Mac OS X, Linux, Windows 7
 # as well as Raspberry Pi Zero, 1,2, and 3.
 #
+PROJECT = mkslides
+
+VERSION = $(shell grep -m 1 'Version =' $(PROJECT).go | cut -d\"  -f 2)
+
+BRANCH = $(shell git branch | grep '* ' | cut -d\  -f 2)
+
 
 build:
 	env CGO_ENABLED=0 go build -o bin/mkslides cmds/mkslides/mkslides.go
@@ -14,19 +20,19 @@ test:
 	go test
 
 install:
-	env GOBIN=$(HOME)/bin go install cmds/mkslides/mkslides.go
+	env CGO_ENABLED=0 GOBIN=$(HOME)/bin go install cmds/mkslides/mkslides.go
 
 status:
 	git status
 
 save:
 	git commit -am "Quick save"
-	git push origin master
+	git push origin $(BRANCH)
 
 clean:
 	if [ -d bin ]; then rm -fR bin; fi
 	if [ -d dist ]; then rm -fR dist; fi
-	if [ -f mkslides-release.zip ]; then rm mkslides-release.zip; fi
+	if [ -f $(PROJECT)-$(VERSION)-release.zip ]; then rm $(PROJECT)-$(VERSION)-release.zip; fi
 
 release:
 	./mk-website.bash
--- mk-release.bash
+++ mk-release.bash
@@ -2,7 +2,10 @@
 #
 # Make releases for Linux/amd64, Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
-REPONAME=mkslides
+PROJECT=mkslides
+VERSION=$(grep -m 1 'Version =' $PROJECT.go | cut -d\" -f 2)
+RELEASE_NAME=$PROJECT-$VERSION
+echo "Preparing release $RELEASE_NAME"
 for PROGNAME in mkslides; do
   env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
@@ -10,5 +13,9 @@ for PROGNAME in mkslides; do
   env GOOS=windows GOARCH=amd64 go build -o dist/windows/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
   env GOOS=darwin	GOARCH=amd64 go build -o dist/maxosx/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 done
-
-zip -r $REPONAME-release.zip README.md INSTALL.md LICENSE dist/* 
+mkdir -p dist
+for FNAME in README.md LICENSE INSTALL.md; do
+  cp -v $FNAME dist/
+done
+echo "Zipping $RELEASE_NAME"
+zip -r $RELEASE_NAME-release.zip dist/* 
